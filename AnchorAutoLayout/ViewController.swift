553b8661f31e493e12bf342725196b3e6f8d9734
Rajat Monga <rajatmonga@Rajats-MacBook-Pro.local>
2018-02-05 19:27:55 -0600

    added green and blue segmented view

--- AnchorAutoLayout/ViewController.swift
+++ AnchorAutoLayout/ViewController.swift
@@ -16,32 +16,82 @@ class ViewController: UIViewController {
         let redView = UIView()
         redView.backgroundColor = .red
         
-        view.addSubview(redView)
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
         
-        //emables auto layout
-        redView.translatesAutoresizingMaskIntoConstraints = false
+        let greenView = UIView()
+        greenView.backgroundColor = .green
         
-        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        [redView, blueView, greenView].forEach{ view.addSubview($0)}
         
-        redView.anchor()
         
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 12), size: .init(width: 125, height: 0))
+        
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        
+        blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0))
+        
+        blueView.anchorSize(to: redView)
+        
+        greenView.anchor (top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
+        
+        
+//        let purpleView = UIView()
+//        purpleView.backgroundColor = .purple
+//
+//        view.addSubview(purpleView)
+        
+//        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+//                       leading: view.safeAreaLayoutGuide.leadingAnchor,
+//                       bottom: view.safeAreaLayoutGuide.bottomAnchor,
+//                       trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16), size: .init(width: 100, height: 100))
+//
     }
 
+    
 
 }
 
 
+
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor) {
+    
+    func fillSuperView() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor )
+        
+    }
+    
+    func anchorSize(to view: UIView) {
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
-        topAnchor.constraint(equalTo: top).isActive = true
-        leadingAnchor.constraint(equalTo: leading).isActive = true
-        bottomAnchor.constraint(equalTo: bottom).isActive = true
-        trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        if let top = top{
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
     
     
