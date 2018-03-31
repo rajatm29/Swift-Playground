199d279bb1500d21762e5a6ce6533611a6006a75
Rajat Monga <rajatmonga29@gmail.com>
2018-03-31 15:07:13 -0500

    Update ChainAnimation.swift

--- ChainAnimation.swift
+++ ChainAnimation.swift
@@ -1,1 +1,25 @@
+import UIKit
 
+class ViewController: UIViewController {
+  
+    let titleLabel = UILabel()
+    let bodyLabel = UILabel()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        titleLabel.backgroundColor = .red
+        bodyLabel.backgroundColor = .green
+        
+        titleLabel.text = "TITLE"
+        bodyLabel.text = "BODY"
+      
+        let stackView = UIStackView (ArrangedSubviews: [titleLabel, bodyLabel])
+      
+        stackView.axis = .vertical
+        stackView.frame = CGRect(x: 0, y: 0, width: 200, height: 400)
+        view.addSubview(stackView)
+    }
+  
+}
