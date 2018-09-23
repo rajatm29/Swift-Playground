f3da6efa6ade6ebe9aef4f8c48f5233e5cb96920
Rajat Monga <rajatmonga29@gmail.com>
2018-09-23 11:53:45 -0500

    Update RandomShuffle.swift

--- RandomShuffle.swift
+++ RandomShuffle.swift
@@ -4,14 +4,23 @@ import UIKit
 // given an array generate a random permuation of the array using the new random number functions
 
 let numbers = [3,1,5,2]
-shuffledArray(numbers)
+
 
 
 func shuffledArray(nums: [Int]) -> [Int] {
+    var copyOfNums = nums
+    var shuffledArray = [Int]() //blank array 
+    
+    while(copyOfNums.count > 0) {
+        let randomIndex = Int.random(in: 0..copyOfNums.count)
+        let removedItem = copyOfNums.remove(at: randomIndex)
+        shuffledArray.append(removedItem)
+    }
     
     //nums.shuffled()
     
-    return []
+    return shuffledArray
     
 }
 
+shuffledArray(nums: numbers)
