c9ff982a6840e24db54dfee2358c7e18608c1946
Rajat Monga <rajatmonga29@gmail.com>
2018-09-23 11:47:41 -0500

    Update and rename memeMachine.swift to RandomShuffle.swift

---
+++ RandomShuffle.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+// given an array generate a random permuation of the array using the new random number functions
+
+let numbers = [3,1,5,2]
+shuffledArray(numbers)
+
+
+func shuffledArray(nums: [Int]) -> [Int] {
+    
+    //nums.shuffled()
+    
+    return []
+    
+}
+
