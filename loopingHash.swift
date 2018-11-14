0e1af965a763a09de1ce09ec21befa17863fa87e
Rajat Monga <rajatmonga29@gmail.com>
2018-11-14 10:22:05 -0600

    Update loopingHash.swift

--- loopingHash.swift
+++ loopingHash.swift
@@ -2,8 +2,21 @@ import UIKit
 
 var sampleString = "1000"
 
+
+//let valueMap = []
+
 func convert(string: String) -> Int {
-    return 0
+    var total = 0
+    
+    for (i, c) in string.enumerated() {
+        let exponent = string.count - i - 1
+        if let value = valueMap[c] {
+            let num = Decimal(value) * pow(10, exponent)
+            total += NSDecimalNumber(decimal:  num).intValue
+        } else {
+            return nil
+        }
+    }
 }
   
   
